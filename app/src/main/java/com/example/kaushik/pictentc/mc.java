6e780c454d9714dad5a903ad8c6f2045fe72a22c
Kaushik Daspute <kaushik.daspute98@gmail.com>
2018-09-09 10:38:28 +0530

    SE TE

---
+++ app/src/main/java/com/example/kaushik/pictentc/mc.java
@@ -0,0 +1,16 @@
+package com.example.kaushik.pictentc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class mc extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mc);
+
+    }
+}
--- app/src/main/java/com/example/kaushik/pictentc/te.java
+++ app/src/main/java/com/example/kaushik/pictentc/te.java
@@ -1,7 +1,9 @@
 package com.example.kaushik.pictentc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class te extends AppCompatActivity {
 
@@ -10,4 +12,14 @@ public class te extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_te);
     }
+    public void onClick(View view)
+    {
+        if(view.getId()==R.id.mc)
+        {
+            Intent intent=new Intent(this,mc.class);
+            startActivity(intent);
+        }
+
+    }
+
 }
