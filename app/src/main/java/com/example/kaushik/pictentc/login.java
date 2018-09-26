d5962bcdbb2f0536850b0b7ce15bf47a9e5973d7
Atom <gehanchopade28@gmail.com>
2018-09-26 18:07:07 +0530

    Firebase login + teachers login done REM:more fields in database

--- app/src/main/java/com/example/kaushik/pictentc/login.java
+++ app/src/main/java/com/example/kaushik/pictentc/login.java
@@ -71,6 +71,7 @@ public class login extends AppCompatActivity implements View.OnClickListener {
                     Intent intent=new Intent(login.this, Front.class);
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);//back does not come to login
                     startActivity(intent);
+                    finish();
 
                 } else {
                     Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_LONG).show();
@@ -78,6 +79,48 @@ public class login extends AppCompatActivity implements View.OnClickListener {
             }
         });
     }
+    private void teachersLogin() {
+        String mail=username.getText().toString().trim();
+        String passw=password.getText().toString().trim();
+        if (username.getText().toString().isEmpty()) {
+            username.setError("Enter E-mail address");
+            username.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(mail).matches())
+        {
+            username.setError("Enter valid E-mail address");
+            username.requestFocus();
+            return;
+        }
+        if (password.getText().toString().isEmpty()) {
+            password.setError("Enter Password");
+            password.requestFocus();
+            return;
+        }
+        if (password.getText().toString().length() != 0 && password.getText().toString().length() < 7) {
+            password.setError("Password should have minimum six characters or symbol or digit");
+            password.requestFocus();
+            return;
+        }
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.signInWithEmailAndPassword(mail,passw).addOnCompleteListener(this,new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if(task.isSuccessful()){
+                    Intent intent=new Intent(login.this, Teachers_login.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);//back does not come to login
+                    startActivity(intent);
+                    finish();
+
+                } else {
+                    Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
     @Override
     public void onClick(View view) {
         switch (view.getId()){
@@ -96,11 +139,9 @@ public class login extends AppCompatActivity implements View.OnClickListener {
             }
             case R.id.login:
             {
-                if((username.getText().toString().trim().equals("KD"))&&(password.getText().toString().trim().equals("123456")))
+                if((username.getText().toString().trim().equals("teachers.entc@pict.edu")))
                 {
-                    Intent intent=new Intent(this,Teachers_login.class);
-                    startActivity(intent);
-                    finish();
+                    teachersLogin();
                 }
                 else
                 {
@@ -111,6 +152,8 @@ public class login extends AppCompatActivity implements View.OnClickListener {
 
         }
     }
+
+
 
 
 
@@ -145,40 +188,7 @@ public class login extends AppCompatActivity implements View.OnClickListener {
 
 
         }*/
-    private boolean check()
-    {
-        int k=0,g=1;
-        if (username.getText().toString().isEmpty()) {
-            username.setError("Enter username");
-            k++;
-        }
-        if(username.getText().toString().startsWith("E2K"))
-        {
-            g=0;
-        }
-        if(g!=0 && username.getText().toString().length()!=0)
-        {
-            username.setError("Enter VALID username");
-            k++;
-        }
-        if (password.getText().toString().isEmpty()) {
-            password.setError("Enter Password");
-            k++;
-        }
-        if (username.getText().toString().length() != 11) {
-            username.setError("Enter valid Enrollment number");
-            k++;
-        }
-        if (password.getText().toString().length() != 0 && password.getText().toString().length() < 7) {
-            Toast.makeText(this, "Password doesn't match", Toast.LENGTH_SHORT).show();
-            k++;
-        }
-        if(k==0)
-        {
-            return true;
-        }
-        else return false;
-    }
+
 
 
 }
