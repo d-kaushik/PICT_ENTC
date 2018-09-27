458f2d6ab731eb65cb4cf9a418492684a0155f05
Atom <gehanchopade28@gmail.com>
2018-09-27 18:17:23 +0530

    Firebase verification added REM:more fields in database     login error

---
+++ app/src/main/java/com/example/kaushik/pictentc/User.java
@@ -0,0 +1,14 @@
+package com.example.kaushik.pictentc;
+
+public class User {
+    public String name,mail,phone,enroll;
+    public User(){
+
+    }
+    public User(String name, String mail, String phone, String enroll){
+        this.name=name;
+        this.mail=mail;
+        this.phone=phone;
+        this.enroll=enroll;
+    }
+}
